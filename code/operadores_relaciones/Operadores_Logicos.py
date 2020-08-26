043d895a15621714d6e411f8f6348ea873be6028
Juan David Espitia <juandespitia14@gmail.com>
2020-08-26 18:35:33 -0500

    update code relational operators

---
+++ code/operadores_relaciones/Operadores_Logicos.py
@@ -0,0 +1,34 @@
+# Operadores boleanos
+print(2+4 == 8)
+print(2+4 == 6)
+
+# Existen tres tipos de operadores logicos
+# NOT
+# AND
+# OR
+print(not True == False)
+print(not True == True)
+print("*********** AND ***********")
+print(True and True)
+print(True and False)
+print(False and False)
+print("*********** OR ***********")
+print(True or True)
+print(True or False)
+print(False or False)
+
+numero = 10
+print("*********** Comparacion numero ***********")
+print(numero > 5 and numero < 15)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ code/operadores_relaciones/Operadores_Relacionales.py
@@ -0,0 +1,28 @@
+# Para comparar dos valores
+3 == 3
+print(3 == 2)
+# Distinto a
+print(3 != 8)
+print(3 != 3)
+
+# Mayor o menor
+print(3 < 8)
+print(3 > 8)
+
+# Mayor o menor igual
+print(5 <= 8)
+print(7 >= 8)
+
+texto = 'Juan'
+print(texto[0]=='J')
+print(texto[1]=='a')
+
+# Comparacion con listas
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+print(lista1 == lista2)
+print(len(lista1) == len(lista2))
+
+
+
+
